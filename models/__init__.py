c71bd6559593c57773591b550439eccd7936d4b9
EivMeyer <eiv.meyer@gmail.com>
2024-09-06 09:29:04 +0200

    Dynamic loading in get_model_class

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,22 @@
-from models.predictive_model_v8 import PredictiveModelV8
-from models.vq_vae_predictive_model import VQVAEPredictiveModel
-from models.autoencoder_model_v0 import AutoEncoderModelV0
-from models.autoencoder_model_v1 import AutoEncoderModelV1
-from models.base_predictive_model import BasePredictiveModel
+import importlib
+import os
 from typing import Type
+from models.base_predictive_model import BasePredictiveModel
 
-def get_model_class(model_type) -> Type[BasePredictiveModel]:
-    model_classes = {
-        "PredictiveModelV8": PredictiveModelV8,
-        "VQVAEPredictiveModel": VQVAEPredictiveModel,
-        "AutoEncoderModelV0": AutoEncoderModelV0,
-        "AutoEncoderModelV1": AutoEncoderModelV1
-        # Add more models here ...
-    }
+def get_model_class(model_type: str) -> Type[BasePredictiveModel]:
+    model_classes = {}
+    models_dir = os.path.dirname(__file__)
+
+    for filename in os.listdir(models_dir):
+        if filename.endswith('.py') and not filename.startswith('__'):
+            module_name = filename[:-3]  # Remove .py extension
+            module = importlib.import_module(f'models.{module_name}')
+            
+            for attribute_name in dir(module):
+                attribute = getattr(module, attribute_name)
+                if (isinstance(attribute, type) and 
+                    issubclass(attribute, BasePredictiveModel) and 
+                    attribute is not BasePredictiveModel):
+                    model_classes[attribute_name] = attribute
+
     return model_classes.get(model_type)
