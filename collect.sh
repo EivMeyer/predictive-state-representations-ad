414dbcdc158ad238c90228d905e87ecd67ef37e1
tdupuis <tom.dupuis@cea.fr>
2024-11-29 23:12:20 +0100

    Minor changes confiig + batch scripts

--- collect.sh
+++ collect.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #SBATCH --time=24:00:00
-#SBATCH -p cpufat
+#SBATCH -p bigmem
 #SBATCH -N 1
-#SBATCH --cpus-per-task=128
+#SBATCH --cpus-per-task=72
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=747G
+#SBATCH --exclusive
 #SBATCH --job-name=collect_crgeo
 #SBATCH --output=logs/%x-%j.txt
 
@@ -13,10 +13,13 @@ set -e
 # Since you're on CPU partition, you probably don't need this
 # export CUDA_VISIBLE_DEVICES=0
 
-# Remove --nv flag if you don't need GPU support
+# # Remove --nv flag if you don't need GPU support
 srun singularity exec \
     --bind $(pwd):/app/psr-ad \
     --bind $(pwd)/output:/app/psr-ad/output \
     --bind /home/users/tdupuis/codes/crgeo_scenarios:/app/psr-ad/scenarios \
     /home/users/tdupuis/codes/psr-ad_latest.sif \
-    ./parallel_dataset_collection.sh -e 1000000 -w 128 -r 1000000
+    python3 change_dataset_batch_size.py ./output/dataset/ ./output/dataset2 100000
+    
+    # ./parallel_dataset_collection.sh dataset.collect_from_trajectories=true commonroad.async_reset=false -e 100000 -w 72 -r 100000
+
---
+++ pretrain.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu80G,gpu40G,prismgpup
+#SBATCH -N 1
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=373G
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=prad-pretrain
+#SBATCH --output=logs/%x-%j.txt
+
+set -e
+
+CUDA_VISIBLE_DEVICES=0
+
+# Remove --nv flag if you don't need GPU support
+srun singularity exec --nv \
+    --bind $(pwd):/app/psr-ad \
+    --bind $(pwd)/output:/app/psr-ad/output \
+    --bind /home/users/tdupuis/codes/crgeo_scenarios:/app/psr-ad/scenarios \
+    /home/users/tdupuis/codes/psr-ad_latest.sif \
+    python3 train_model.py wandb.offline=true
